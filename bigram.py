479842a3d6ecae0acbc661c2b671e122f11597bf
NainaniJatinZ <nainani.jatin.0@gmail.com>
2023-07-05 23:01:29 +0530

    nanogpt script complete

--- bigram.py
+++ bigram.py
@@ -95,7 +95,113 @@ class BigramLanguageModel(nn.Module):
             # append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
+
+# class BigramLanguageModel_old2(nn.Module):
+
+#     def __init__(self):
+#         super().__init__()
+#         # each token directly reads off the logits for the next token from a lookup table
+#         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+#         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+#         self.blocks = nn.Sequential(
+#             Block(n_embd, n_head=4),
+#             Block(n_embd, n_head=4),
+#             Block(n_embd, n_head=4),
+#             nn.LayerNorm(n_embd),
+#         )
+#         self.lm_head = nn.Linear(n_embd, vocab_size)
+
+#     def forward(self, idx, targets=None):
+
+#         B, T = idx.shape
+#         # idx and targets are both (B,T) tensor of integers
+#         tok_emb = self.token_embedding_table(idx) # (B,T,C)  c = n_embd
+#         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T,C)
+#         x = tok_emb + pos_emb # token identities AND positions at which corresponding tokens occur
+#         # broadcasting note -> Will be batched pos_emb of (T, C) will be (1, T, C) for each batch
+#         x = self.blocks(x)
+#         logits = self.lm_head(x) # (B,T,vocab_size)
+
+#         if targets is None:
+#             loss = None
+#         else:
+#             B, T, C = logits.shape
+#             logits = logits.view(B*T, C)
+#             targets = targets.view(B*T)
+#             loss = F.cross_entropy(logits, targets)
+
+#         return logits, loss
+
+#     def generate(self, idx, max_new_tokens):
+#         # idx is (B, T) array of indices in the current context
+#         for _ in range(max_new_tokens):
+#             # crop idx to the last block size
+#             idx_cond = idx[:,-block_size:]
+#             # get the predictions
+#             logits, loss = self(idx_cond)
+#             # focus only on the last time step
+#             logits = logits[:, -1, :] # becomes (B, C)
+#             # apply softmax to get probabilities
+#             probs = F.softmax(logits, dim=-1) # (B, C)
+#             # sample from the distribution
+#             idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+#             # append sampled index to the running sequence
+#             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+#         return idx
     
+
+
+# class BigramLanguageModel_old(nn.Module):
+
+#     def __init__(self):
+#         super().__init__()
+#         # each token directly reads off the logits for the next token from a lookup table
+#         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+#         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+#         self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8 dimensional self attention
+#         self.ffwd = FeedFoward(n_embd)
+#         self.lm_head = nn.Linear(n_embd, vocab_size)
+
+#     def forward(self, idx, targets=None):
+
+#         B, T = idx.shape
+#         # idx and targets are both (B,T) tensor of integers
+#         tok_emb = self.token_embedding_table(idx) # (B,T,C)  c = n_embd
+#         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T,C)
+#         x = tok_emb + pos_emb # token identities AND positions at which corresponding tokens occur
+#         # broadcasting note -> Will be batched pos_emb of (T, C) will be (1, T, C) for each batch
+#         x = self.sa_heads(x) # apply one head of self-attention (B,T,n_embd)
+#         x = self.ffwd(x) 
+
+#         logits = self.lm_head(x) # (B,T,vocab_size)
+
+#         if targets is None:
+#             loss = None
+#         else:
+#             B, T, C = logits.shape
+#             logits = logits.view(B*T, C)
+#             targets = targets.view(B*T)
+#             loss = F.cross_entropy(logits, targets)
+
+#         return logits, loss
+
+#     def generate(self, idx, max_new_tokens):
+#         # idx is (B, T) array of indices in the current context
+#         for _ in range(max_new_tokens):
+#             # crop idx to the last block size
+#             idx_cond = idx[:,-block_size:]
+#             # get the predictions
+#             logits, loss = self(idx_cond)
+#             # focus only on the last time step
+#             logits = logits[:, -1, :] # becomes (B, C)
+#             # apply softmax to get probabilities
+#             probs = F.softmax(logits, dim=-1) # (B, C)
+#             # sample from the distribution
+#             idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+#             # append sampled index to the running sequence
+#             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+#         return idx
+
 model = BigramLanguageModel(vocab_size)
 m = model.to(device)
 
